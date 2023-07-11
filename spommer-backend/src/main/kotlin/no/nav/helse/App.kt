b550dd281509063bf32215fe644211a2f8dcb806
Helene Arnesen <helene.arnesen@nav.no>
2023-07-11 12:30:21 +0200

    :bug: fjerner database midlertidig

--- spommer-backend/src/main/kotlin/no/nav/helse/App.kt
+++ spommer-backend/src/main/kotlin/no/nav/helse/App.kt
@@ -9,14 +9,14 @@ fun main() {
     val logger = LoggerFactory.getLogger("spommer")
     logger.info("Hello spommer!")
 
-    val konfig = Konfig.fromEnv()
-    val dataSourceBuilder = DataSourceBuilder(konfig)
-    val app = App(dataSourceBuilder)
+    //val konfig = Konfig.fromEnv()
+    //val dataSourceBuilder = DataSourceBuilder(konfig)
+    val app = App()
     app.startBlocking()
 }
 
 internal class App(
-    private val database: DataSourceBuilder
+    //private val database: DataSourceBuilder
 ) {
     //private val bordDao = BordDao(database.getDataSource())
 
